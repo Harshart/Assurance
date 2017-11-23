47733bdd857a582dc0a24be417a6ed73eee1f0e7
olivi <olivi@MSI>
2017-11-23 15:25:47 +0100

    Test grosse bite

---
+++ ProjetActuariat/src/projetactuariat/ProjetActuariat.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetactuariat;
+
+/**
+ *
+ * @author olivi
+ */
+public class ProjetActuariat {
+    GROSSE BITE
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
